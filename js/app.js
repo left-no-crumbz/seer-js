7e5122d749fbb59e41bbf327fd29743088277914
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-06-10 22:58:43 +0800

    feat: testing

--- js/app.js
+++ js/app.js
@@ -3,11 +3,16 @@ const searchBar = document.getElementById("search-box");
 
 
 const googleWebCache = "https://webcache.googleusercontent.com/search?q=cache:";
+const archiveCache = "https://archive.is/?run=1&url=";
+
+// TODO: 
+// - Use diff caches
+// - improve styling prolly
+// - endgame how to display the text instead of just redirecting
 
 submitBtn.addEventListener("click", (event) => {
-    const targetURL = searchBar.value;
-    const url = encodeURI(`${googleWebCache}${targetURL}`);
     event.preventDefault();
-    console.log(url);
+    const targetURL = searchBar.value;
+    const url = encodeURI(`${archiveCache}${targetURL}`);
     window.location.replace(url);
 });
