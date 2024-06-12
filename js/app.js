544bd9dd03c7d0c3cc616d2e00257af5b711563c
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-06-12 12:33:29 +0800

    feat: Tryna make it as a PWA

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 const submitBtn = document.getElementById("submit-btn");
 const searchBar = document.getElementById("search-box");
-
+const iframe = document.getElementById("iframe");
 
 const googleWebCache = "https://webcache.googleusercontent.com/search?q=cache:";
-const archiveCache = "https://archive.is/?run=1&url=";
+const archiveCache = "https://archive.is/submit/?url=";
 
 // TODO: 
 // - Use diff caches
@@ -14,5 +14,5 @@ submitBtn.addEventListener("click", (event) => {
     event.preventDefault();
     const targetURL = searchBar.value;
     const url = encodeURI(`${archiveCache}${targetURL}`);
-    window.location.replace(url);
+    window.open(url, "_blank");
 });
